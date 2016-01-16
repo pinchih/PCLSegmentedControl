17b20ccd01f4665f6142d84c8595c90a48e4da44
Pin-Chih Lin <tobeaceo@gmail.com>
2016-01-15 16:01:47 -0800

    Initial Commit

---
+++ PCLSegmentedControl/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PCLSegmentedControl
+//
+//  Created by Pin-Chih on 1/15/16.
+//  Copyright © 2016 Pin-Chih. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PCLSegmentedControl/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PCLSegmentedControl
+//
+//  Created by Pin-Chih on 1/15/16.
+//  Copyright © 2016 Pin-Chih. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
